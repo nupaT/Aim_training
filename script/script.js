4007fbf554a1eb67774a6c6cf5dc31cd134bcdf4
nupaT <trash_jack@mail.ru>
2021-09-28 14:52:12 +0300

    add: function change timer

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ const startBtn = document.querySelector("#start");
 const screens = document.querySelectorAll(".screen");
 const timeList = document.querySelector("#time-List");
 let time = 0;
+const timeEnd = document.querySelector("#time");
 
 startBtn.addEventListener("click", (event) => {
   event.preventDefault();
@@ -11,10 +12,24 @@ startBtn.addEventListener("click", (event) => {
 timeList.addEventListener("click", (event) => {
   if (event.target.classList.contains("time-btn")) {
     time = +event.target.getAttribute("data-time");
+    screens[1].classList.add("up");
     startGame();
   }
 });
 
 function startGame() {
-  screens[1].classList.add("up");
+  setInterval(changeTime, 1000);
+  setTimeout(time);
+}
+
+function changeTime() {
+  let current = --time;
+  if (current < 10) {
+    current = `0${current}`;
+  }
+  setTimeout(current);
+}
+
+function setTimeout(value) {
+  timeEnd.innerHTML = `00:${value}`;
 }
