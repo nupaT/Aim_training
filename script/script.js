c4a66e3e94fbf9453a6440af451c431f3d00fda8
nupaT <trash_jack@mail.ru>
2021-09-28 15:33:36 +0300

    add: funcctional for random create circle, random position for circle, increment score after click on circle

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@ const screens = document.querySelectorAll(".screen");
 const timeList = document.querySelector("#time-List");
 let time = 0;
 const timeEnd = document.querySelector("#time");
+const board = document.querySelector("#board");
+let score = 0;
 
 startBtn.addEventListener("click", (event) => {
   event.preventDefault();
@@ -20,16 +22,55 @@ timeList.addEventListener("click", (event) => {
 function startGame() {
   setInterval(changeTime, 1000);
   setTimeout(time);
+  createRandomCircle();
 }
 
 function changeTime() {
-  let current = --time;
-  if (current < 10) {
-    current = `0${current}`;
+  if (time === 0) {
+    finishGame();
+  } else {
+    let current = --time;
+    if (current < 10) {
+      current = `0${current}`;
+    }
+    setTimeout(current);
   }
-  setTimeout(current);
 }
+
+board.addEventListener("click", (event) => {
+  if (event.target.classList.contains("circle")) {
+    score++;
+    console.log(score);
+    event.target.remove();
+    createRandomCircle();
+  }
+});
 
 function setTimeout(value) {
   timeEnd.innerHTML = `00:${value}`;
+}
+
+function finishGame() {
+  timeEnd.parentNode.classList.add("hide");
+  board.innerHTML = `<h1>Ваш счет: <span class="primary">${score}</span> </h1>`;
+}
+
+function createRandomCircle() {
+  const circle = document.createElement("div");
+  const size = randomCircleSize(10, 60);
+  // деструктуризация
+  const { width, height } = board.getBoundingClientRect();
+  const x = randomCircleSize(0, width - size);
+  const y = randomCircleSize(0, height - size);
+
+  circle.classList.add("circle");
+  circle.style.width = `${size}px`;
+  circle.style.height = `${size}px`;
+  circle.style.top = `${y}px`;
+  circle.style.left = `${x}px`;
+  board.append(circle);
+}
+
+function randomCircleSize(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
